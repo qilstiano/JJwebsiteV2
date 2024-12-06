314f3eb67b35fdd12be86ad2917527b568cc6d83
jiawendh <90663992+jiawendh@users.noreply.github.com>
2024-12-07 02:53:02 +0800

    fix about me mobile ver

--- nextjs_app/app/about/page.jsx
+++ nextjs_app/app/about/page.jsx
@@ -90,11 +90,11 @@ export default function AboutUs() {
     <main className="w-full pt-8 flex flex-col bg-gradient-to-r from-light-violet to-extra-light-violet min-h-screen">
       <section className="bg-[url('/images/AboutUs/OurVisionMasthead.jpg')] bg-cover bg-center relative">
         <div className='bg-[#674C93AD] absolute top-0 bottom-0 right-0 left-0 z-20'></div>
-        <div className='max-w-6xl mx-auto relative z-30 min-h-[350px] flex flex-col justify-center'>
+        <div className='max-w-6xl mx-auto relative z-30 min-h-[350px] flex flex-col justify-center px-5'>
           <h2 className='font-bold text-white text-[72px]'>Our Vision:</h2>
           <h1 className='font-bold text-white text-[50px]'>A Catalyst for the Empowerment of changemakers</h1>
         </div>
-        <div className='max-w-6xl mx-auto flex relative z-30 gap-1 w-full'>
+        <div className='max-w-6xl mx-auto flex flex-wrap relative z-30 gap-1 w-full'>
           {Nav.map((item, index) => {
             return (
               <a index={index} href={`#${item}`} className='text-white text-base text-center bg-[#674C93] hover:bg-[#7A5BAD] py-1.5 px-3 border-2 border-white rounded-lg grow'>{item}</a>
@@ -103,10 +103,10 @@ export default function AboutUs() {
         </div>
       </section>
 
-      <section className='max-w-6xl mx-auto flex gap-10 items-start pt-8 pb-6'>
-        <Image alt="About Us Image" className='' src={`/images/AboutUs/AboutUs.png`} width={500} height={500} />
-        <div className='font-normal text-white text-base flex flex-col gap-5 w-[60%]'>
-          <p className='font-semibold text-white text-[36px] pb-3'>A message from our Founders,</p>
+      <section className='max-w-6xl mx-auto lg:flex gap-10 items-start pt-8 pb-6 px-5'>
+        <Image alt="About Us Image" className='w-[80%] mx-auto mb-8 lg:m-0 lg:w-[40%]' src={`/images/AboutUs/AboutUs.png`} width={500} height={500} />
+        <div className='font-normal text-white text-base flex flex-col gap-5 w-full lg:w-[60%]'>
+          <p className='font-semibold text-white text-4xl pb-3'>A message from our Founders,</p>
           <p>We've been driven by a lifelong quest for impactful education and societal change.</p>
           <p>Having dedicated years to volunteering and running Comm.UnitySG, we saw firsthand the transformative power of youth initiatives. However, we recognized a critical gap: many young minds lacked engagement with social issues due to traditional educational barriers.</p>
           <p>That's where Jalan Journey began. Inspired by our experiences, we envisioned a platform where learning about social issues wasn't just educational but also engaging and empowering. By gamifying Citizenship and Character Education (CCE), we've revolutionized how students interact with complex societal challenges.</p>
@@ -118,10 +118,10 @@ export default function AboutUs() {
       </section>
 
       <section className='bg-[#836AA4]'>
-        <div className='max-w-6xl mx-auto text-center pt-16 pb-6'>
+      <div className='max-w-6xl mx-auto text-center pt-16 pb-6 px-5'>
           <h6 className='text-white text-[46px] font-bold'>Meet the Team</h6>
           <p className='text-white text-[28px] font-normal'>For Students, by Students</p>
-          <div className='grid grid-cols-5 gap-8 mt-16'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 mt-16'>
             {Team.map((member, index) => {
               return (
                 <div index={index} className='flex flex-col items-center'>
@@ -135,10 +135,10 @@ export default function AboutUs() {
       </section>
 
       <section className='bg-[#674C93]'>
-        <div className='max-w-6xl mx-auto text-center pt-16 pb-6'>
+        <div className='max-w-6xl mx-auto text-center pt-16 pb-6 px-5'>
           <h6 className='text-white text-[46px] font-bold'>Meet Our Advisors</h6>
           <p className='text-white text-[28px] font-normal'>A wealth of experience and subject matter expertise</p>
-          <div className='grid grid-cols-5 gap-8 mt-16'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 mt-16'>
             {Team.map((member, index) => {
               return (
                 <div index={index} className='flex flex-col items-center'>
@@ -152,7 +152,7 @@ export default function AboutUs() {
       </section>
 
       <section className='bg-gradient-to-b from-[#866BB1] to-[#B8ACC8]'>
-        <div className='max-w-6xl mx-auto text-center pt-16'>
+        <div className='max-w-6xl mx-auto text-center pt-16 px-5'>
           <h6 className='text-white text-[46px] font-bold pb-8'>Our Values</h6>
           <Image alt="Our Values Image" className='w-full' src={`/images/AboutUs/OurValues.png`} width={1000} height={500} />
         </div>
@@ -164,9 +164,9 @@ export default function AboutUs() {
         </div>
         <Carousel className="max-w-3xl w-2/3 mx-auto">
           <CarouselContent className="ml-0">
-            <CarouselItem className="w-full mx-auto bg-[#F1E8DB] flex gap-5 rounded-md p-5 border-[#306054] border-l-8 border-b-8 mb-10 relative">
-              <Image alt="Our Journey Image" className='w-1/2' src={`/images/AboutUs/OurJourneyImage.jpg`} width={400} height={250} />
-              <div className='w-full flex flex-col relative'>
+            <CarouselItem className="w-full mx-auto bg-[#F1E8DB] md:flex gap-5 rounded-md p-5 border-[#306054] border-l-8 border-b-8 mb-10 relative">
+              <Image alt="Our Journey Image" className='w-full md:w-1/2 self-center' src={`/images/AboutUs/OurJourneyImage.jpg`} width={400} height={250} />
+              <div className='w-full flex flex-col relative pb-12 pt-5 md:pt-0'>
                 <p className='font-bold text-[28px] text-white rounded-md bg-[#306054] px-4 py-1 self-center'>2022</p>
                 <ul className='text-black flex flex-col gap-2 ml-5 uppercase mt-5'>
                   <li className='list-disc font-bold'>Founded in October 2022</li>
@@ -179,9 +179,9 @@ export default function AboutUs() {
               <div className='absolute top-full left-0 border-[#306054] border-t-[40px] border-x-[10px] border-x-transparent translate-x-2.5'></div>
             </CarouselItem>
 
-            <CarouselItem className="w-full mx-auto bg-[#F1E8DB] flex gap-5 rounded-md p-5 border-[#306054] border-l-8 border-b-8 mb-10 relative">
-              <Image alt="Our Journey Image" className='w-1/2' src={`/images/AboutUs/OurJourneyImage.jpg`} width={400} height={250} />
-              <div className='w-full flex flex-col relative'>
+            <CarouselItem className="w-full mx-auto bg-[#F1E8DB] md:flex gap-5 rounded-md p-5 border-[#306054] border-l-8 border-b-8 mb-10 relative">
+              <Image alt="Our Journey Image" className='w-full md:w-1/2 self-center' src={`/images/AboutUs/OurJourneyImage.jpg`} width={400} height={250} />
+              <div className='w-full flex flex-col relative pb-12 pt-5 md:pt-0'>
                 <p className='font-bold text-[28px] text-white rounded-md bg-[#306054] px-4 py-1 self-center'>2023</p>
                 <ul className='text-black flex flex-col gap-2 ml-5 uppercase mt-5'>
                   <li className='list-disc font-bold'>CYZ</li>
@@ -194,9 +194,9 @@ export default function AboutUs() {
               <div className='absolute top-full left-1/3 -translate-x-1 border-[#306054] border-t-[40px] border-x-[10px] border-x-transparent translate-x-2.5'></div>
             </CarouselItem>
 
-            <CarouselItem className="w-full mx-auto bg-[#F1E8DB] flex gap-5 rounded-md p-5 border-[#306054] border-l-8 border-b-8 mb-10 relative">
-              <Image alt="Our Journey Image" className='w-1/2' src={`/images/AboutUs/OurJourneyImage.jpg`} width={400} height={250} />
-              <div className='w-full flex flex-col relative'>
+            <CarouselItem className="w-full mx-auto bg-[#F1E8DB] md:flex gap-5 rounded-md p-5 border-[#306054] border-l-8 border-b-8 mb-10 relative">
+              <Image alt="Our Journey Image" className='w-full md:w-1/2 self-center' src={`/images/AboutUs/OurJourneyImage.jpg`} width={400} height={250} />
+              <div className='w-full flex flex-col relative pb-12 pt-5 md:pt-0'>
                 <p className='font-bold text-[28px] text-white rounded-md bg-[#306054] px-4 py-1 self-center'>2024</p>
                 <ul className='text-black flex flex-col gap-2 ml-5 uppercase mt-5'>
                   <li className='list-disc font-bold'>CYZ</li>
