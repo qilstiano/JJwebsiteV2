233ba0adbfafaacbf3599d73eda5424d6ad9246c
a'qil <131641361+qilstiano@users.noreply.github.com>
2025-01-05 23:58:58 +0800

    feat/navbar

--- nextjs_app/app/components/general/Navbar.jsx
+++ nextjs_app/app/components/general/Navbar.jsx
@@ -18,59 +18,62 @@ const Navbar = () => {
             "url('/images/Header.svg'), linear-gradient(180deg, rgba(8,71,56,1) 31%, rgba(103,77,147,1) 100%)",
         }}
       >
-        {/* Logo */}
-        <a
-          href="/"
-          className="flex items-center justify-center p-0 -m-1 w-[70px] h-[70px]"
-          style={{ display: "inline-block" }}
-        >
-          <Image
-            src="/images/JJlogo.svg"
-            alt="Jalan Journey"
-            width={100}
-            height={100}
-            className="w-full h-auto"
-          />
-        </a>
-
-        {/* Menu and Dropdown */}
-        <div className="absolute pl-14 order-1 flex items-center">
-          <div
-            className="cursor-pointer hover:bold"
-            onClick={toggleDropdown}
+        {/* Logo and Menu Container */}
+        <div className="flex items-center">
+          {/* Logo */}
+          <a
+            href="/"
+            className="flex items-center justify-center z-2"
+            style={{ width: '70px', height: 'auto' }}
           >
-            &#9776; Menu
-          </div>
-          {dropdownOpen && (
-            <div className="absolute top-[3.5rem] left-[4.5rem] bg-gray-800 bg-opacity-80 text-white rounded shadow-lg z-10">
-              <a href="/about" className="block px-4 py-2 hover:bg-gray-600">
-                About Us
-              </a>
-              <a href="/faq" className="block px-4 py-2 hover:bg-gray-600">
-                FAQ
-              </a>
-              <a
-                href="/testimonials"
-                className="block px-4 py-2 hover:bg-gray-600"
-              >
-                Testimonials
-              </a>
-              <a href="/joinus" className="block px-4 py-2 hover:bg-gray-600">
-                Get Involved
-              </a>
-              <a
-                href="/contactus"
-                className="block px-4 py-2 hover:bg-gray-600"
-              >
-                Contact Us
-              </a>
+            <Image
+              src="/images/JJlogo.svg"
+              alt="Jalan Journey"
+              width={100}
+              height={100}
+              className="h-auto"
+            />
+          </a>
+
+          {/* Menu and Dropdown */}
+          <div className="relative flex items-center">
+            <div
+              className="cursor-pointer hover:bold"
+              onClick={toggleDropdown}
+            >
+              &#9776; Menu
             </div>
-          )}
+            {dropdownOpen && (
+              <div className="absolute top-[4.5rem] bg-gray-800 bg-opacity-80 text-white rounded shadow-lg z-10">
+                <a href="/about" className="block px-4 py-2 hover:bg-gray-600">
+                  About Us
+                </a>
+                <a href="/faq" className="block px-4 py-2 hover:bg-gray-600">
+                  FAQ
+                </a>
+                <a
+                  href="/testimonials"
+                  className="block px-4 py-2 hover:bg-gray-600"
+                >
+                  Testimonials
+                </a>
+                <a href="/joinus" className="block px-4 py-2 hover:bg-gray-600">
+                  Get Involved
+                </a>
+                <a
+                  href="/contactus"
+                  className="block px-4 py-2 hover:bg-gray-600"
+                >
+                  Contact Us
+                </a>
+              </div>
+            )}
+          </div>
         </div>
 
         {/* Links centered according to viewport */}
         <div className="absolute left-1/2 transform -translate-x-1/2 flex space-x-4">
-          <a href="/institutions" className="hover:underline text-sm px-">
+          <a href="/institutions" className="hover:underline text-sm">
             Institutions
           </a>
           <a href="/organisations" className="hover:underline text-sm">
